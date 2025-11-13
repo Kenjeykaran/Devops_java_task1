5ed4f04ddc9966e5cd6c6cd220b1408f560f887e
Kenjeykaran <masskenjey@gmail.com>
2025-11-13 12:02:31 +0000

    edit javafile

--- src/main/java/com/devops/javaapp/DevopsJavaApplication.java
+++ src/main/java/com/devops/javaapp/DevopsJavaApplication.java
@@ -1,18 +1,14 @@
 package com.devops.javaapp;
- 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
- 
 @SpringBootApplication
 @RestController
 public class DevopsJavaApplication {
- 
     public static void main(String[] args) {
         SpringApplication.run(DevopsJavaApplication.class, args);
     }
- 
     /**
      * This method handles HTTP GET requests to the root URL ("/").
      * @return A simple string that will be displayed in the browser.
