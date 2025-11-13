b371c91d7fd06ae589493f853b584ce461b12b13
Kenjeykaran <masskenjey@gmail.com>
2025-11-13 11:56:24 +0000

    removed app.java and docker

--- app.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World from Jenkins Webhook!");
-        System.output.println("Second line seven edit :");
-    }
-}
--- dockerfile
+++
@@ -1,8 +0,0 @@
-FROM openjdk:17-jdk-slim
-
-COPY target/myapp.jar /app/myapp.jar
-
-WORKDIR  /app
-
-ENTRYPOINT ["java", "-jar", "myapp.jar"]
-
